9fbf685a041f8c8a69cd415a06a97dd458f328a2
samuelwstout <samuelwstout@gmail.com>
2022-10-05 15:33:25 -0500

    Install react-router-dom

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Hello. This is Vite/React in Rails!</h1>
+      <h1>Hello! This is Vite/React in Rails!</h1>
     </div>
   );
 };
--- app/javascript/entrypoints/index.jsx
+++ app/javascript/entrypoints/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 import App from '../components/App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
