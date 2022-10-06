2fbd7413779e5ab0587430bc496e8f2609bb8c0c
samuelwstout <samuelwstout@gmail.com>
2022-10-05 23:06:32 -0500

    create models and their code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
+    def as_json(options={})
+        super(options.merge({ methods: :type }))
+    end
+
 end
