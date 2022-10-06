3e4cd2ade569291698496a8122c332833728c413
samuelwstout <samuelwstout@gmail.com>
2022-10-05 23:05:19 -0500

    configure middleware for cookies

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,13 @@ module RubyRailsPostgres
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protection = :strict
+    
     config.hosts << "freenote.up.railway.app"
   end
 end
