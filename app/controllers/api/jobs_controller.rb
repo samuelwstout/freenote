2f0dca90c9e6cf0a982a452780f9f94569f54e6e
samuelwstout <samuelwstout@gmail.com>
2022-10-09 15:31:22 -0500

    create action jobs build

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,2 +1,16 @@
 class Api::JobsController < ApplicationController
+
+    skip_before_action :verify_authenticity_token
+
+    def create
+        job = Job.create(job_params)
+        render json: job, status: :created
+    end
+
+    private
+
+    def job_params
+        params.permit(:title, :description, :date, :location, :budget, :contractor_id)
+    end
+
 end
