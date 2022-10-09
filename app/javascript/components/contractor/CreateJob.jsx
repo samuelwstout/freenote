87cb1146d1805f0d9838ed157714bd51406e5007
samuelwstout <samuelwstout@gmail.com>
2022-10-09 15:29:47 -0500

    build job post request

--- app/javascript/components/contractor/CreateJob.jsx
+++ app/javascript/components/contractor/CreateJob.jsx
@@ -1,10 +1,16 @@
-import React, {useEffect} from 'react'
+import React, { useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import NavBarContractor from '../nav/NavBarContractor'
 
-const CreateJob = ({setCurrentUser, currentUser}) => {
+const CreateJob = ({setCurrentUser, currentUser, setJobs, jobs}) => {
 
 const navigate = useNavigate();
+
+const [title, setTitle] = useState('')
+const [description, setDescription] = useState('')
+const [date, setDate] = useState('')
+const [location, setLocation] = useState('')
+const [budget, setBudget] = useState('')
 
 useEffect(() => {
   if (currentUser) {
@@ -14,20 +20,57 @@ useEffect(() => {
     if (Object.keys(currentUser).length === 0) {
       navigate('/')
     }
-  }
+  } 
 }, [currentUser])
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  
+  fetch('/api/jobs', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title,
+      description,
+      date,
+      location,
+      budget,
+      contractor_id: currentUser.id
+    })
+  })
+  .then((r) => {
+    if (r.ok) {
+      r.json().then(data => {
+        const newJobs = [...jobs, data]
+        setJobs(newJobs)
+      })
+    } else {
+      r.json().then(error => {
+        console.log(error)
+      })
+    }
+  })
+
+  setTitle('')
+  setDescription('')
+  setDate('')
+  setLocation('')
+  setBudget('')
+}
 
   return (
     <div>
       <NavBarContractor setCurrentUser={setCurrentUser} />
       <h1>Create Job Post</h1>
-        <form>
-            <input placeholder='Headline'></input>
-            <input placeholder='Description'></input>
-            <input placeholder='Date(s)'></input>
-            <input placeholder='Location'></input>
-            <input placeholder='Budget'></input>
-            <input type='submit'></input>
+        <form onSubmit={handleSubmit}>
+            <input placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} />
+            <input placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)} />
+            <input placeholder='Date(s)' type='date' value={date} onChange={(e) => setDate(e.target.value)} />
+            <input placeholder='Location' value={location} onChange={(e) => setLocation(e.target.value)} />
+            <input placeholder='Budget' value={budget} onChange={(e) => setBudget(e.target.value)} />
+            <input type='submit' />
         </form>
     </div>
   )
