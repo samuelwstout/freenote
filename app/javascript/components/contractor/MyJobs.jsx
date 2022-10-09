a70c2a99223547ef4a4fc878eb6be25b4eb627c8
samuelwstout <samuelwstout@gmail.com>
2022-10-09 15:33:07 -0500

    display jobs

--- app/javascript/components/contractor/MyJobs.jsx
+++ app/javascript/components/contractor/MyJobs.jsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBarContractor from '../nav/NavBarContractor';
 
-const MyJobs = ({currentUser, setCurrentUser}) => {
+const MyJobs = ({currentUser, setCurrentUser, jobs}) => {
 
   const navigate = useNavigate();
 
@@ -21,7 +21,19 @@ const MyJobs = ({currentUser, setCurrentUser}) => {
     <div>
         <NavBarContractor setCurrentUser={setCurrentUser} />
         <h1>My Jobs</h1>
-        <h2>Jobs go here</h2>
+        {jobs && 
+          jobs.map(job => {
+            return (
+              <div key={job.id}>
+                <h3>{job.title}</h3>
+                <h3>{job.description}</h3>
+                <h3>{job.date}</h3>
+                <h3>{job.location}</h3>
+                <h3>${job.budget}</h3>
+              </div>
+            )
+          })
+        }
     </div>
   )
 }
