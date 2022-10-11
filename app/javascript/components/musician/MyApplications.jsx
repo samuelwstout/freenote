c54901bdde838faa976869f63c36f8dbd14c6685
samuelwstout <samuelwstout@gmail.com>
2022-10-11 13:06:41 -0500

    render jobs from jobApplications

--- app/javascript/components/musician/MyApplications.jsx
+++ app/javascript/components/musician/MyApplications.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBarMusician from '../nav/NavBarMusician';
 
-const MyApplications = ({ setCurrentUser, currentUser }) => {
-
+const MyApplications = ({ setCurrentUser, currentUser, jobs, jobApplications }) => {
+    
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -16,13 +16,30 @@ const MyApplications = ({ setCurrentUser, currentUser }) => {
       }
     }
   }, [currentUser])
+ 
+  const jobAppsFromCurrentUser = jobApplications.filter(jobApp => jobApp.musician_id === currentUser.id)
+
+  const includesID = (id) => {
+    const onFilter = jobAppsFromCurrentUser.filter((jobApp) => jobApp.job_id == id);
+
+    return onFilter.length > 0 ? true : false;
+  };
 
   return (
     <div>
        <NavBarMusician setCurrentUser={setCurrentUser}/>
-        <h2>My applications</h2>
-        <h3>Headline           status: insert status</h3>
-        <p>So on...</p>
+        <h1>My applications</h1>
+        <div>
+             {jobs.map((job) => {
+              if (includesID(job.id)) {
+                return (
+                  <div key={job.id}>
+                    <h1>{job.title}</h1>
+                  </div>
+                );
+              }
+            })}
+        </div>
     </div>
   )
 }
