c615547170f238b1731f79304830ac136f7d2ea8
samuelwstout <samuelwstout@gmail.com>
2022-10-12 15:33:03 -0500

    remove conditionals in user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,12 +4,7 @@ class Api::UsersController < ApplicationController
   # get '/api/me'
   def show
     if current_user
-      if current_user.type == "Contractor"
-        render json: current_user, include: :jobs, status: :ok
-      end
-      if current_user.type == "Musician"
-        render json: current_user, include: :job_applications, status: :ok
-      end
+        render json: current_user, status: :ok
     else
       render json: { error: 'No active session' }, status: :unauthorized
     end
