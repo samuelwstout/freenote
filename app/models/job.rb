1c9777df9b1eb49333ba4b386b76c5447ca5c984
samuelwstout <samuelwstout@gmail.com>
2022-10-21 13:42:07 -0500

    add dependent: :destroy

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ApplicationRecord
     belongs_to :contractor
-    has_many :job_applications
+    has_many :job_applications, dependent: :destroy
     has_many :musicians, through: :job_applications
 end
--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,5 +1,5 @@
 class JobApplication < ApplicationRecord
     belongs_to :job
     belongs_to :musician
-    has_one :application_response
+    has_one :application_response, dependent: :destroy
 end
