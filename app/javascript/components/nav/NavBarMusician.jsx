5de5c2ae75d42b42707b3d95bb0cfeebbf86c1c9
samuelwstout <samuelwstout@gmail.com>
2022-10-23 16:11:31 -0500

    build musician navbar

--- app/javascript/components/nav/NavBarMusician.jsx
+++ app/javascript/components/nav/NavBarMusician.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { useNavigate } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
 const NavBarMusician = ({setCurrentUser}) => {
 
   const navigate = useNavigate();
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const findWorkClick = () => {
+    setAnchorElNav(null);
+    navigate('/find-work')
+  }
+
+  const appliedJobsClick = () => {
+    setAnchorElNav(null);
+    navigate('/applied-jobs')
+  }
 
   const handleLogout = () => {
     fetch('/api/logout', {
@@ -19,15 +60,126 @@ const NavBarMusician = ({setCurrentUser}) => {
   }
 
   return (
-    <div>
-      <ul>
-        <li><Link to="/find-work">Find Work</Link></li>
-        <li><Link to="/applied-jobs">Applied Jobs</Link></li>
-        <li><Link to="/musician-profile">Profile</Link></li>
-        <p><button onClick={handleLogout}>Logout</button></p>
-      </ul>
-    </div>
-  )
-}
+    <AppBar position="static">
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              mb: .3,
+              display: { xs: 'none', md: 'flex' },
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Freenote
+          </Typography>
 
-export default NavBarMusician
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+                <MenuItem onClick={findWorkClick}>
+                  <Typography textAlign="center">Find Work</Typography>
+                </MenuItem>
+                <MenuItem onClick={appliedJobsClick}>
+                  <Typography textAlign="center">Applied Jobs</Typography>
+                </MenuItem>
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Freenote
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Button
+                onClick={findWorkClick}
+                sx={{ my: 2, color: 'white', display: 'block', textTransform: 'none' }}
+              >
+                Find Work
+              </Button>
+              <Button
+                onClick={appliedJobsClick}
+                sx={{ my: 2, color: 'white', display: 'block', textTransform: 'none' }}
+              >
+                Applied Jobs
+              </Button>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar src="/broken-image.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography onClick={() => navigate('/musician-profile')} textAlign="center">Profile</Typography>
+                </MenuItem>
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography onClick={handleLogout} textAlign="center">Logout</Typography>
+                </MenuItem>
+            </Menu>
+          </Box>  
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default NavBarMusician;
