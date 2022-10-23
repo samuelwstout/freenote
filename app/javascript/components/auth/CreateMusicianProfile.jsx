49db2bee36983abeca80c9ecf6abb0c5772d8885
samuelwstout <samuelwstout@gmail.com>
2022-10-23 12:52:32 -0500

    style create musician profile

--- app/javascript/components/auth/CreateMusicianProfile.jsx
+++ app/javascript/components/auth/CreateMusicianProfile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import {Avatar, Button, CssBaseline, TextField, Box, Typography, Container} from '@mui/material'
 
 const CreateMusicianProfile = ({currentUser, setMusicianProfile}) => {
 
@@ -18,6 +19,7 @@ const CreateMusicianProfile = ({currentUser, setMusicianProfile}) => {
       const [bio, setBio] = useState('')
       const [mediaUrl, setMediaUrl] = useState('')
       const [email, setEmail] = useState('')
+      const [error, setError] = useState('')
 
       const handleSubmit = (e) => {
         e.preventDefault();
@@ -43,7 +45,7 @@ const CreateMusicianProfile = ({currentUser, setMusicianProfile}) => {
               })
             } else {
               res.json().then(errors => {
-                console.error(errors)
+                setError(errors.error)
               })
             }
           })
@@ -55,7 +57,99 @@ const CreateMusicianProfile = ({currentUser, setMusicianProfile}) => {
       }
 
   return (
-    <div>
+    <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <Box
+      sx={{
+        marginTop: 4.5,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Complete your profile
+      </Typography>
+      {error && 
+        <Typography variant="subtitle1" color="error.dark">
+        {error}
+      </Typography>
+      }
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="location"
+          label="Location"
+          name="location"
+          autoComplete="location"
+          autoFocus
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="instrument"
+          label="Instrument"
+          name="instrument"
+          autoComplete="instrument"
+          autoFocus
+          onChange={(e) => setInstrument(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="bio"
+          label="Bio"
+          type="bio"
+          id="bio"
+          autoComplete="bio"
+          onChange={(e) => setBio(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="media"
+          label="Youtube link of you performing"
+          type="media"
+          id="media"
+          autoComplete="media"
+          onChange={(e) => setMediaUrl(e.target.value)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  </Container>
+  )
+}
+
+export default CreateMusicianProfile
+
+{/* <div>
         <form onSubmit={handleSubmit}>
             <h1>Create Musician Profile</h1>
             <p>
@@ -80,8 +174,4 @@ const CreateMusicianProfile = ({currentUser, setMusicianProfile}) => {
             </p>
             <input type='submit' />
         </form>
-    </div>  
-  )
-}
-
-export default CreateMusicianProfile
+    </div>  */}
--- app/javascript/components/auth/SignupAsMusician.jsx
+++ app/javascript/components/auth/SignupAsMusician.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import {Avatar, Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container} from '@mui/material'
 
 const SignupAsMusician = ({setCurrentUser, currentUser}) => {
 
@@ -21,6 +22,7 @@ const [lastName, setLastName] = useState('')
 const [username, setUsername] = useState('')
 const [password, setPassword] = useState('')
 const [passwordConfirmation, setPasswordConfirmation] = useState('')
+const [error, setError] = useState('')
 
 const handleSubmit = (e) => {
     e.preventDefault()
@@ -46,7 +48,7 @@ const handleSubmit = (e) => {
           })
         } else {
           res.json().then(errors => {
-            console.error(errors)
+            setError(errors.error)
           })
         }
       })
@@ -58,34 +60,129 @@ const handleSubmit = (e) => {
     }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h1>Sign up to find work</h1>
-        <p>
-          <label htmlFor='firstname'>First Name </label>
-          <input type="text" name="first_name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        </p>
-        <p>
-          <label htmlFor='lastname'>Last Name </label>
-          <input type="text" name="last_name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-        </p>
-        <p>
-          <label htmlFor='username'>Username </label>
-          <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </p>
-        <p>
-          <label htmlFor='password'>Password </label>
-          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </p>
-        <p>
-        <label htmlFor='password_confirmation'>Password Confirmation </label>
-        <input type="password" name="password_confirmation" value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} />
-        </p>
-        <input type='submit' />
-      </form>
-      <h4>Already have an account? <button><Link to="/signin">Sign in</Link></button></h4>
-    </div>
+  <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <Box
+      sx={{
+        marginTop: 4.5,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign up to find work
+      </Typography>
+      {error && 
+        <Typography variant="subtitle1" color="error.dark">
+        {error}
+      </Typography>
+      }
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="first_name"
+          label="First name"
+          name="first_name"
+          autoComplete="first_name"
+          autoFocus
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="last_name"
+          label="Last name"
+          name="last_name"
+          autoComplete="last_name"
+          autoFocus
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          autoComplete="username"
+          autoFocus
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password_confirmation"
+          label="Password Confirmation"
+          type="password_confirmation"
+          id="password_confirmation"
+          autoComplete="password-confirmation"
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </Button>
+        <Grid container justifyContent="center">
+          <Grid item>
+            <Link onClick={() => navigate('/signin')} variant="body2">
+              {"Already have an account? Sign in"}
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  </Container>
   )
 }
 
 export default SignupAsMusician
+
+{/* <div>
+<form onSubmit={handleSubmit}>
+  <h1>Sign up to find work</h1>
+  <p>
+    <label htmlFor='firstname'>First Name </label>
+    <input type="text" name="first_name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+  </p>
+  <p>
+    <label htmlFor='lastname'>Last Name </label>
+    <input type="text" name="last_name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+  </p>
+  <p>
+    <label htmlFor='username'>Username </label>
+    <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+  </p>
+  <p>
+    <label htmlFor='password'>Password </label>
+    <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+  </p>
+  <p>
+  <label htmlFor='password_confirmation'>Password Confirmation </label>
+  <input type="password" name="password_confirmation" value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} />
+  </p>
+  <input type='submit' />
+</form>
+<h4>Already have an account? <button><Link to="/signin">Sign in</Link></button></h4>
+</div> */}
