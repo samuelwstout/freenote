6add51e978510038022319573ef982291502387a
samuelwstout <samuelwstout@gmail.com>
2022-10-24 08:48:15 -0500

    style applied jobs

--- app/javascript/components/musician/AppliedJobs.jsx
+++ app/javascript/components/musician/AppliedJobs.jsx
@@ -1,6 +1,14 @@
-import React, {useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
-import NavBarMusician from '../nav/NavBarMusician';
+import React, {useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import NavBarMusician from '../nav/NavBarMusician'
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
 
 const AppliedJobs = ({ setCurrentUser, currentUser, jobs, jobApplications }) => {
     
@@ -26,26 +34,48 @@ const AppliedJobs = ({ setCurrentUser, currentUser, jobs, jobApplications }) => 
 
   return (
     <div>
-       <NavBarMusician setCurrentUser={setCurrentUser}/>
-        <h1>Jobs You've Applied To</h1>
-        <div>
+       <NavBarMusician setCurrentUser={setCurrentUser} />
+       <Box
+          sx={{
+            pb: 4
+          }}
+        >
+        <Container sx={{ py: 6 }} maxWidth="md">
              {jobs.map((job) => {
               if (includesID(job.id)) {
                 return (
-                  <div key={job.id}>
-                    <ul>
-                      <li>{job.title}</li>
-                      <li>{job.description}</li>
-                      <li>{job.date}</li>
-                      <li>{job.location}</li>
-                      <li>${job.budget}</li>
-                      <button onClick={() => navigate(`/view-application/job/${job.id}`)}>View your application</button>
-                    </ul>
-                  </div>
-                );
+                  <Grid item key={job.id} xs={12} sm={6} md={4}>
+                    <Card
+                    sx={{ height: '100%', display: 'flex', flexDirection: 'column'}}
+                    variant="outlined"
+                    >
+                    <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    {job.title}
+                    </Typography>
+                    <Typography>
+                    {job.description}
+                    </Typography>
+                    <Typography>
+                      {job.date}
+                    </Typography>
+                    <Typography>
+                      {job.location}
+                    </Typography>
+                    <Typography>
+                      ${job.budget}
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small" onClick={() => navigate(`/view-application/job/${job.id}`)}>View your application</Button>
+                    </CardActions>
+                    </Card>
+                  </Grid>
+                )
               }
             })}
-        </div>
+        </Container>
+        </Box>
     </div>
   )
 }
