fcdd347587b100d23f06d186f6617ce58e1b46ea
samuelwstout <samuelwstout@gmail.com>
2022-10-24 08:48:07 -0500

    style find work

--- app/javascript/components/musician/FindWork.jsx
+++ app/javascript/components/musician/FindWork.jsx
@@ -1,6 +1,14 @@
 import React, {useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBarMusician from '../nav/NavBarMusician';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const FindWork = ({currentUser, setCurrentUser, jobs}) => {
   
@@ -19,27 +27,50 @@ const FindWork = ({currentUser, setCurrentUser, jobs}) => {
 
   const mapJobs = jobs.map(job => {
     return (
-      <div key={job.id}>
-        <ul>
-          <li>{job.title}</li>
-          <li>{job.description}</li>
-          <li>{job.date}</li>
-          <li>{job.location}</li>
-          <li>${job.budget}</li>
-          <button onClick={() => navigate(`/job/${job.id}`)}>Apply</button>
-        </ul>
-      </div>
+      <Grid item key={job.id} xs={12} sm={6} md={4}>
+        <Card
+          sx={{ height: '100%', display: 'flex', flexDirection: 'column'}}
+          variant="outlined"
+        >
+          <CardContent sx={{ flexGrow: 1 }}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {job.title}
+            </Typography>
+            <Typography>
+              {job.description}
+            </Typography>
+            <Typography>
+              {job.date}
+            </Typography>
+            <Typography>
+              {job.location}
+            </Typography>
+            <Typography>
+              ${job.budget}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" onClick={() => navigate(`/job/${job.id}`)}>Apply</Button>
+          </CardActions>
+        </Card>
+      </Grid>
     )
   })
-
-
 
   return (
     <div>
         <NavBarMusician setCurrentUser={setCurrentUser} />
+        <Box
+          sx={{
+            pb: 4,
+          }}
+        >
+        <Container sx={{ py: 6 }} maxWidth="md">
         {jobs.length !== 0 &&
           mapJobs
         } 
+        </Container>
+        </Box> 
     </div>
     )
 }
