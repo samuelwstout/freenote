f5be229920aaf18d214c5f33bd782ae4cbf6489e
samuelwstout <samuelwstout@gmail.com>
2022-10-26 23:13:21 -0500

    mui accordion for musician-profile

--- app/javascript/components/musician/MusicianProfile.jsx
+++ app/javascript/components/musician/MusicianProfile.jsx
@@ -1,14 +1,23 @@
-import React, {useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
-import NavBarMusician from '../nav/NavBarMusician';
-import { Box, Container, Card, Typography, Button } from '@mui/material'
+import React, {useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import NavBarMusician from '../nav/NavBarMusician'
+import { Box, Container, Typography, Accordion, AccordionDetails, AccordionSummary } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
-const MusicianProfile = ({setCurrentUser, currentUser, musicians, musicianProfile}) => {
+const MusicianProfile = ({setCurrentUser, currentUser, musicians }) => {
+
+  const [expanded, setExpanded] = React.useState(false)
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false)
+  }
 
   let musician;
+  let profile;
 
   if (Object.keys(currentUser).length !== 0) {
     musician = musicians.find(item => item.id === currentUser.id)
+    profile = musician.musician_profile
   }
 
   const navigate = useNavigate();
@@ -24,52 +33,150 @@ const MusicianProfile = ({setCurrentUser, currentUser, musicians, musicianProfil
     }
   }, [currentUser])
 
+
   return (
     <div>
       <NavBarMusician setCurrentUser={setCurrentUser} />
         <Box>
-        <Container sx={{ py: 3 }} maxWidth="md">
+        <Container sx={{ py: 6 }} maxWidth="md">
         {musician && 
-        <div>
-          <Card>
-          <Typography gutterBottom variant="h6" component="h2">First name: {musician.first_name}</Typography>
-          </Card>
-          <Card sx={{ mt: 2 }}>
-          <Typography gutterBottom variant="h6" component="h2">Last name: {musician.last_name}</Typography>
-          </Card>
-          <Card sx={{ mt: 2 }}>
-          <Typography gutterBottom variant="h6" component="h2">Username: {musician.username}</Typography>
-          </Card>
-          {Object.keys(musicianProfile).length !== 0 && 
           <div>
-            <h3>Email: {musicianProfile.email}</h3>
-            <h3>Location: {musicianProfile.location}</h3>
-            <h3>Instrument: {musicianProfile.instrument}</h3>
-            <h3>Bio: {musicianProfile.bio}</h3>
-            <h3>Media Url: {musicianProfile.media_url}</h3>
+             <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            First name
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}>{musician.first_name}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+            Aliquam eget maximus est, id dignissim quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Last name</Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            {musician.last_name}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
+            varius pulvinar diam eros in elit. Pellentesque convallis laoreet
+            laoreet.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>
+            Username
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            {musician.username}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+            amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
           </div>
-          }
-          {musician.musician_profile &&
-          <div>
-            <Card sx={{ mt: 2 }}>
-            <Typography gutterBottom variant="h6" component="h2">Email: {musician.musician_profile.email}</Typography>
-            </Card>
-            <Card sx={{ mt: 2 }}>
-            <Typography gutterBottom variant="h6" component="h2">Location: {musician.musician_profile.location}</Typography>
-            </Card>
-            <Card sx={{ mt: 2 }}>
-            <Typography gutterBottom variant="h6" component="h2">Instrument: {musician.musician_profile.instrument}</Typography>
-            </Card>
-            <Card sx={{ mt: 2}}>
-            <Typography gutterBottom variant="h6" component="h2">Bio: {musician.musician_profile.bio}</Typography>
-            </Card>
-            <Card sx={{ mt: 2}}>
-            <Typography gutterBottom variant="h6" component="h2">Media Url: {musician.musician_profile.media_url}</Typography>
-            </Card>
+      }
+      {profile &&
+      <div>
+      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4bh-content"
+          id="panel4bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Email</Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            {profile.email}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+            amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel5bh-content"
+          id="panel5bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Location</Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            {profile.location}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+            amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel6bh-content"
+          id="panel6bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Instrument</Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            {profile.instrument}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+            amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel7bh-content"
+          id="panel7bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0 }}>Bio</Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            {profile.bio}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+            amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+        {/* profile.media_url */}
           </div>
-          }
-        </div>
-        }
+      }
         </Container>
         </Box>
     </div>
