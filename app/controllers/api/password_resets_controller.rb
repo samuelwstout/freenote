bd56026ba579845714f06843a135cb58a41b9401
samuelwstout <samuelwstout@gmail.com>
2022-11-04 11:06:24 -0500

    pw reset controller method

--- app/controllers/api/password_resets_controller.rb
+++ app/controllers/api/password_resets_controller.rb
@@ -1,2 +1,10 @@
 class Api::PasswordResetsController < ApplicationController
+
+    def create
+        user = User.find_by_email(params[:email])
+        user.send_password_reset if user
+        flash[:notice] = 'Email sent with password reset instructions.'
+        redirect_to new_session_path
+    end
+    
 end
