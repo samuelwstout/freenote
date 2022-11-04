9137918fab04cb3ba503d02a9055ce088283c6fd
samuelwstout <samuelwstout@gmail.com>
2022-11-04 11:05:44 -0500

    add user attributes for password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_035536) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_155615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "timescaledb"
@@ -65,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_035536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
   add_foreign_key "application_responses", "job_applications"
