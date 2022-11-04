5bf4483ff2ce365b01b67198ae46487a5da6d4f1
samuelwstout <samuelwstout@gmail.com>
2022-11-04 11:06:37 -0500

    create user mailer file and its methods

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+
+    def forgot_password(user)
+        @user = user
+        @greeting = "Hi"
+
+        mail to: user.email, :subject => 'Reset password instructions'
+    end
+end
