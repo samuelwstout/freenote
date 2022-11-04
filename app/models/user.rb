933287d1e88c71a364c61732da12f3a8a4859c74
samuelwstout <samuelwstout@gmail.com>
2022-11-04 11:06:03 -0500

    user model methods for pw reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,19 @@ class User < ApplicationRecord
         super(options.merge({ methods: :type }))
     end
 
+    # sends an email with a link for the user to reset password
+    def send_password_reset
+        generate_token(:password_reset_token)
+        self.password_reset_sent_at = Time.zone.now
+        save!
+        UserMailer.forgot_password(self).deliver
+    end
+
+    # generates random password reset token
+    def generate_token(column)
+        begin
+            self[column] = SecureRandom.urlsafe_base64
+        end while User.exists?(column => self[column])
+    end
+
 end
