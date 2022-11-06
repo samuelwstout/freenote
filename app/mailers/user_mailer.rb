0cb692cff9417b8646ce8d1d33df91800b33fa43
samuelwstout <samuelwstout@gmail.com>
2022-11-06 10:41:34 -0600

    build welcome email action

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+
+  def welcome_email
+    @user = params[:user]
+    @url  = 'https://freenote.up.railway.app'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
 end
