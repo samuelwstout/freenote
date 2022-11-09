0ecfdfed2839a97071a2d91276a6b5b6ce75b367
samuelwstout <samuelwstout@gmail.com>
2022-11-08 18:31:31 -0600

    make syntax cleaner

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def registration_confirmation(user)
     @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
+    mail :to => user.email, :subject => "Registration Confirmation - Freenote"
   end
 
   def password_reset(user)
