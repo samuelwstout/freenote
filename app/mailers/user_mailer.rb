4c8d3a7bbc96b96c7024f1bab3d88e691e7442ac
samuelwstout <samuelwstout@gmail.com>
2022-11-08 18:26:35 -0600

    build registration user_mailer method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
+  def registration_confirmation(user)
+    @user = user
+    mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
+  end
+
   def password_reset(user)
     @user = user
     mail to: user.email, subject: "Password reset - Freenote"
