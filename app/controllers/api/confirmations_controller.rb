6c9bd69bf4ff814796882bda46b4c3131b7f3d11
samuelwstout <samuelwstout@gmail.com>
2022-11-10 12:50:21 -0600

    confirmation controller actions

--- app/controllers/api/confirmations_controller.rb
+++ app/controllers/api/confirmations_controller.rb
@@ -1,2 +1,18 @@
 class Api::ConfirmationsController < ApplicationController
+    skip_before_action :confirm_authentication
+
+    def request
+        current_user.send_email_confirmation
+    end
+
+    def submit
+        user = User.find_by(confirm_token: params[:confirm_token], email: params[:email])
+        if user
+            user.confirm_email
+            session[:user_id] = user.id
+        else
+            render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+        end    
+    end
+
 end
