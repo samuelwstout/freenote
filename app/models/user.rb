355af8f44e97bdf53a7a85a8a5c0a7a2aecfef95
samuelwstout <samuelwstout@gmail.com>
2022-11-11 22:05:08 -0600

    remove confirm_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,21 +33,11 @@ class User < ApplicationRecord
         UserMailer.email_confirmation(self).deliver_now
     end
 
-    def confirm_email
-        user = User.find_by_confirm_token(params[:confirm_token])
-        if user
-          user.email_activate
-          flash[:success] = "Welcome to the Sample App! Your email has been confirmed.
-          Please sign in to continue."
-        else
-          flash[:error] = "Sorry. User does not exist"
-        end
-    end
 
     def email_activate
         self.email_confirmed = true
         self.confirm_token = nil
-        save!(:validate => false)
+        save!
     end
 
     private
